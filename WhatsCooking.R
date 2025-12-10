3541476c5914ef9352c9315cca6f6ef1c9178f9f
connertilghman <conner.tilghman@gmail.com>
2025-12-10 12:39:12 -0700

    Final WhatsCooking code

--- WhatsCooking.R
+++ WhatsCooking.R
@@ -6,35 +6,75 @@ library(tidyverse)
 library(tidymodels)
 library(textrecipes)
 
-trainSet <- read_file("train.json") |>
-  fromJSON()
-testSet <- read_file("test.json") |>
-  fromJSON()
+trainSet <- fromJSON("train.json") 
+testSet <- fromJSON("test.json") 
 
-## Define TF-IDF9
-rec <- recipe(cuisine ~ ingredients, data = trainSet) |>
-  step_mutate(ingredients = tokenlist(ingredients)) |>
-  step_tokenfilter(ingredients, max_tokens=500) |>
+trainSet$cuisine <- as.factor(trainSet$cuisine)
+
+
+trainSet$num_ingredients <- sapply(trainSet$ingredients, length)
+trainSet$avg_ingredient_length <- sapply(trainSet$ingredients, function(x) mean(nchar(x)))
+trainSet$num_long_ingredients <- sapply(trainSet$ingredients, function(x) sum(sapply(strsplit(x, " "), length) > 1))
+
+testSet$num_ingredients <- sapply(testSet$ingredients, length)
+testSet$avg_ingredient_length <- sapply(testSet$ingredients, function(x) mean(nchar(x)))
+testSet$num_long_ingredients <- sapply(testSet$ingredients, function(x) sum(sapply(strsplit(x, " "), length) > 1))
+
+trainSet$num_unique_words <- sapply(trainSet$ingredients, function(x) length(unique(unlist(strsplit(x, " ")))))
+testSet$num_unique_words  <- sapply(testSet$ingredients, function(x) length(unique(unlist(strsplit(x, " ")))))
+
+trainSet$num_single_word <- sapply(trainSet$ingredients, function(x) sum(sapply(strsplit(x, " "), length) == 1))
+testSet$num_single_word  <- sapply(testSet$ingredients, function(x) sum(sapply(strsplit(x, " "), length) == 1))
+
+
+rec <- recipe(cuisine ~ ingredients + num_ingredients + avg_ingredient_length + num_long_ingredients + num_unique_words + num_single_word,
+              data = trainSet) %>%
+  step_mutate(ingredients = tokenlist(ingredients)) %>%
+  step_tokenfilter(ingredients, max_tokens = 2500) %>%   
   step_tfidf(ingredients)
 
-rf_spec <- rand_forest(
-  mtry = 10,        # number of variables randomly sampled at each split
-  trees = 500,      # number of trees
-  min_n = 5         # minimum samples per leaf
-) |>
-  set_engine("ranger") |>
+
+data_split <- initial_split(trainSet, prop = 0.8, strata = cuisine)
+train_data <- training(data_split)
+valid_data <- testing(data_split)
+
+
+xgb_model <- boost_tree(
+  trees = 250,        
+  tree_depth = 10,     
+  learn_rate = 0.05,  
+  loss_reduction = 0, 
+  sample_size = 0.8   
+) %>%
+  set_engine("xgboost") %>%
   set_mode("classification")
 
-rf_workflow <- workflow() |>
-  add_model(rf_spec) |>
+
+wf <- workflow() %>%
+  add_model(xgb_model) %>%
   add_recipe(rec)
 
-rf_fit <- rf_workflow |> fit(data = trainSet)
+xgb_fit <- wf %>% fit(data = train_data)
 
-test_predictions <- predict(rf_fit, testSet) |>
-  bind_cols(testSet)
+xgb_preds <- predict(xgb_fit, valid_data) %>%
+  bind_cols(valid_data %>% select(cuisine))
 
-submission <- test_predictions |>
-  select(id, cuisine = .pred_class)
+class_metrics <- metric_set(accuracy, kap, f_meas)
 
+class_metrics(
+  xgb_preds,
+  truth = cuisine,
+  estimate = .pred_class
+)
+
+xgb_final <- wf %>% fit(data = trainSet)
+
+test_predictions <- predict(xgb_final, testSet)
+
+submission <- tibble(
+  id = testSet$id,
+  cuisine = test_predictions$.pred_class
+)
+
+     
 write_csv(submission, "submission.csv")
